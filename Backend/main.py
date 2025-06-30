1a0afffb4f5516165020dc33f95f15ff09e242b7
pavankalyan13 <padigampavankalyan13@gmail.com>
2025-06-30 11:35:26 +0530

    Basic repo setup

---
+++ Backend/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/health")
+def health_check():
+    return dict(status="OK")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app,host="0.0.0.0", port=5001)
